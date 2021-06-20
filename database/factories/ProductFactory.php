893d17081a839d8df7ed871e81082f51f42575ad
uddin2019 <phpdeveloper.shakir@gmail.com>
2021-06-20 17:30:23 +0600

    hello world

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,8 +2,10 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\Product;
+use App\Models\SubCategory;
 use Illuminate\Support\Str;
 
 class ProductFactory extends Factory
@@ -24,13 +26,16 @@ class ProductFactory extends Factory
     {
         $title = $this->faker->sentence;
         $slug = Str::slug($title, '-');
+        $catslug = Category::findOrFail(rand(5,8))->slug;
+        $subcatslug = SubCategory::findOrFail(rand(3,10))->slug;
+        $img = ['th_1623144058.jpg','th_1622963747.png','th_1623666350.jpg'];
         return [
             'title'=>$title,
             'slug'=>$title,
             'cate_id'=>rand(5,9),
-            'cat_slug'=>'category-'.rand(1,4),
+            'cat_slug'=>$catslug,
             'subcate_id'=>rand(3,11),
-            'subcat_slug'=>'SubCategory-'.rand(1,8),
+            'subcat_slug'=>$subcatslug,
             'resubcate_id'=>rand(1,7),
             'tag'=>'fdgfd,dsafdsa,dsafds,fsdafdsa,sdafdsa',
             'description'=>$this->faker->paragraph,
@@ -39,7 +44,7 @@ class ProductFactory extends Factory
             'software'=>json_encode(["1","2","3","4"]),
             'link'=>json_encode(["#"]),
             'gallery_img'=>json_encode(["60c6fcd01d0ba.jpg","60c6fcd025f6e.jpg","60c6fcd02a04a.jpg"]),
-            'thumbnail_img'=>'th_1623144058.jpg',
+            'thumbnail_img'=>$img[rand(0,2)],
             'creator_by'=>1,
             'draft'=>1,
             'status'=>1,
